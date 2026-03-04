50b4f4bf1fc081d6d2a97d56f29d91bee373e88d
anant <anant1@iitb.ac.in>
2026-03-04 19:06:34 +0530

    corruption recovery script with sample files

---
+++ corruption-demo/sample-corruption-data.sql
@@ -0,0 +1,4 @@
+update usertable set field1 = 'aaa' where ycsb_key = 1;
+update usertable set field1 = 'aaa' where ycsb_key = 3201;
+update usertable set field1 = 'aaa' where ycsb_key = 6401;
+update usertable set field1 = 'aaa' where ycsb_key = 9601;
